b9a8122374a0853e25e960b131e0f70cb7abc20c
Cristian Garcia <cristiangarc@Cristians-MacBook-Air.local>
2024-11-01 12:34:08 -0400

    add a smiley face to the repaint of the snake

--- snake_game.py
+++ snake_game.py
@@ -6,6 +6,7 @@ print(original)
 
 print('please move up, down, left or right:')
 
+# column, row values are numbered from 1-18, 
 tail = 1
 head = 7
 
@@ -30,11 +31,12 @@ def addDots(head, body):
     # print('head argument in addDots:', head)
     out = ''
     for j in range(len(new_body)):
+        current_pixel = j + 1
         next_index = min(18, j + 1)
         prev_index = max(0, j - 1)
-        if (j == max(0, head - 5)):
+        if (current_pixel == max(1, head - 5)):
             for k in range(6):
-                if (k != 6):
+                if (k < 5):
                     out += '.'
                 else:
                     out += ':O'
@@ -45,30 +47,16 @@ def addDots(head, body):
             out += ' '
         elif (body[j] == '.'):
             out += '.'
-        if (j % 6 == 0 and j != 0 and j != 18 and j != max(0, head - 5)):
+        else:
+            out += body[j]
+        if (j % 6 == 0 and j != 0 and current_pixel != 18 and current_pixel != max(1, head - 5)):
             out += '\n'
     return out
-
-print('addDots:')
-print('123456')
-print(addDots(head, original))
 
 body = ''
 
 length = len(original)
-for i in range(length):
-    current_pixel = i + 1
-    if (current_pixel == head - 5):
-        # add the trailing dots
-        body += '.....:O'
-    
-    elif(original[i] == '.'):
-        body += '.'
-    else:
-        body += ' '
-
-    if (current_pixel % 6 == 0 and current_pixel != 0):
-        body += '\n'
+print(addDots(head, original))
 
 print('123456')
 print(body)
